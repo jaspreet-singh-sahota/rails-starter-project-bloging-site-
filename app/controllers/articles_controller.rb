7653c78e1bbd40c9afbc50ea6aa8b54cfa2cd914
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-04-28 10:17:46 +0530

    delete method created

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -33,7 +33,14 @@ class ArticlesController < ApplicationController
       render 'edit'
     end
   end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
   
+    redirect_to articles_path
+  end
+
   private
     def article_params
       params.require(:article).permit(:title, :text)
